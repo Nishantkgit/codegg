80d90c14703c71a0a17f20fcb48bd3dbe41a417e
Nishantkgit <nkathpalia2002@gmail.com>
2023-04-05 08:43:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import imag from './cloud.png';
+import imag from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 // import ReactS3 from 'react-s3'
-import S3 from 'react-aws-s3'
+import { useEffect, useRef,useState } from 'react';
+import { Storage, Amplify } from 'aws-amplify';
+// import S3 from 'react-aws-s3'
 
-const config={
-  accessKeyId:'AKIARP76KWTXZEGRKVXD',
-  secretAccessKey:'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',
-  region:'ap-south-1',
-  bucketName:'miniproj1',
-}
-function App() {
-  var nme = "Madhavan"
-  const [file, setFile] = useState()
-  const handleFileInput = (e) => {
-    setFile(e.target.files[0]);
-  }
+// const config={
+//   accessKeyId:'AKIARP76KWTXZEGRKVXD',
+//   secretAccessKey:'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',
+//   region:'ap-south-1',
+//   bucketName:'miniproj1',
+// }
+// function App() {
+//   var nme = "Madhavan"
+//   const [file, setFile] = useState()
+//   const handleFileInput = (e) => {
+//     setFile(e.target.files[0]);
+//   }
 
-  const uploadfile=async(file)=> {
-    const ReactS3Client = new S3(config);
-    ReactS3Client.uploadFile(file,file.name).then(data=>{
-      console.log(data.location)
-    })
-  }
+//   const uploadfile=async(file)=> {
+//     const ReactS3Client = new S3(config);
+//     ReactS3Client.uploadFile(file,file.name).then(data=>{
+//       console.log(data.location)
+//     })
+//   }
   // function handleSubmit(event) {
   //   event.preventDefault()
   //   const url = 'http://localhost:3000/uploadFile';
@@ -39,18 +41,88 @@ function App() {
   //   });
 // }
 
+
+
+function App() {
+  const ref = useRef(null);
+  const [files, setFiles] = useState([]);
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState();
+
+  useEffect(() => {
+    Amplify.configure({
+      Auth: {
+        identityPoolId: "us-east-1:167c882a-91ad-429f-a205-717d4f27991f",
+        region: "us-east-1",
+      },
+
+      Storage: {
+        AWSS3: {
+          bucket: "miniproj12",
+          region: "us-east-1",
+        },
+      },
+    });
+  }, []);
+
+  const loadImages = () => {
+    Storage.list("")
+      .then((files) => {
+        console.log(files);
+        setFiles(files);
+      })
+      .catch((err) => {
+        console.log(err);
+      });    
+  }
+
+  useEffect(() => {
+    loadImages();
+   }, []);
+
+  const handleFileLoad = () => {
+    const filename = ref.current.files[0].name;
+    Storage.put(filename, ref.current.files[0], {
+      progressCallback: (progress) => {
+        setProgress(Math.round((progress.loaded / progress.total) * 100) + "%");
+        setTimeout(() => { setProgress() }, 1000);
+      }
+    })
+      .then(resp => {
+      console.log(resp);
+      loadImages();
+    }).catch(err => {console.log(err);});
+  }
+
+  const handleShow = (file) => {
+    Storage.get(file).then(resp => {
+      console.log(resp);
+      setImage(resp)
+    }).catch(err => { console.log(err); });
+  }
+
+  const handleDelete = (file) => {
+    Storage.remove(file).then(resp => {
+      console.log(resp);
+      loadImages();
+     }).catch(err => { console.log(err); });
+  }
+
+
   return (
     <div className="App">
-      <h1>Welcome {nme}</h1>
+      <h1>Welcome Nishant</h1>
       <div className='box'>
         <div className="card one">
           <img src={imag} alt="" />
           <div className="container">
             <h4><b>Upload</b></h4>
             <form id="fileform" >
-              <input type="file" id="myFile" name="filename"onChange={handleFileInput}/>
+              <input ref={ref} type="file" onChange={handleFileLoad}/>
+
+              
               <div className='btn'>
-                <input type="submit" id='subbtn' onClick={()=>uploadfile(file)}/>
+                {/* <input type="submit" id='subbtn' onClick={()=>handleFileLoad}/> */}
               </div>
             </form>
           </div>
